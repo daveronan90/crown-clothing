04e0bd3e968d36d9251ad841f0e4c3a77e8866b8
daveronan <daveronan90@gmail.com>
2021-03-20 15:49:55 +0000

    Updated menu items to nav with with Router()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/HomePage.component";
 
 import "./App.css";
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
